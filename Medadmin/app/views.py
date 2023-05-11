6c696fdab15789c5e00d58d3c6e4ebe32feb0def
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-05-11 01:50:09 +0100

    API data

--- Medadmin/app/views.py
+++ Medadmin/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    
+    # get all doctors
+    url = "https://medico-production-fa1c.up.railway.app/api/all/docs" 
+    
+    # Get doc data
+    response = requests.get(url)
+    data = response.json()
+    
+    return render(request, "basic_files/index.html", {"data":data})
