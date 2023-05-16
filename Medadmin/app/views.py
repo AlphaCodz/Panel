a9732d50b6d4b1ed48adb219eebe01715fadcd2e
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-05-16 12:22:57 +0100

    PATIENT 100

--- Medadmin/app/views.py
+++ Medadmin/app/views.py
@@ -7,33 +7,46 @@ from django.contrib.auth.hashers import check_password
 
 # Create your views here.
 
-import requests
-
 def index(request):
     token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxMDMyMzk3MDgxMiwiaWF0IjoxNjgzOTcwODEyLCJqdGkiOiI1ZjMwNzE4Y2Y3NDg0MGNmYTRmOTUxY2QwYzEzN2I3NiIsInVzZXJfaWQiOjg2fQ.dvgBFacSU6w4z5AHN0MQls6DvKEk-PwrbX1tgikR8Wk"
     headers = {'Authorization' : f'Bearer {token}'}
     
     user_url = "https://medico-production-fa1c.up.railway.app/api/admin/data"
     doc_data_url = "https://medico-production-fa1c.up.railway.app/api/all/docs"
+    patient_url = "https://medico-production-fa1c.up.railway.app/api/all/users"
     
     user_response = requests.get(user_url, headers=headers)
     doc_data_url = requests.get(doc_data_url, headers=headers)
+    patient_response = requests.get(patient_url, headers=headers)
     if user_response.status_code == 200 and doc_data_url.status_code == 200:
         user_data = user_response.json().get('admin')
         doc_data = doc_data_url.json().get('doctors')
-        # Total Number of Doctors
-        
+            # Total Number of Doctors    
         doc_count = len(doc_data)
-        # doc_data_url = 
         context = {
             "user": user_data,
             "docs": doc_data,
-            "total_no_of_docs": doc_count
+            "total_no_of_docs": doc_count,
+            "patients_data": []
         }
-        return render(request, "basic_files/index.html", context)
-    else:
-        print("Error: Failed to Retrieve data")
-        return HttpResponse("Failed to retrieve data")
+        if patient_response.status_code == 200:
+            patients_data = patient_response.json().get('patients')
+            print(patients_data)
+            for patient in patients_data:
+                data = {
+                    "first_name": patient.get('first_name'),
+                    "last_name": patient.get('last_name'),
+                    "email": patient.get('email'),
+                    "username": patient.get('username'),
+                    "is_patient": patient.get('is_patient'),
+                    "is_medic": patient.get('is_medic'),
+                    "is_admin": patient.get('is_admin')
+                }
+                context["patients_data"].append(data)
+            return render(request, "basic_files/index.html", context)
+        else:
+            print("Error: Failed to Retrieve data")
+            return HttpResponse("Failed to retrieve data")
     
     
 def signup(request):
