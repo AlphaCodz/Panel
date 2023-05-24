5e003b7a1738c1808e9dc0532bd33367ecc317e7
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-05-24 01:40:04 +0100

    Dep ready4

--- Medadmin/config/urls.py
+++ Medadmin/config/urls.py
@@ -14,17 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("app/", include("app.urls"))
 ]
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r"^static/(?P<path>.*)$", views.serve),
-    ]
-urlpatterns += staticfiles_urlpatterns()
 
     
